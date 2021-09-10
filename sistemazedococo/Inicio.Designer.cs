5e5bc2f6113d6a74c07ce5eae7b8121fdc48825d
weslley pereira <wpds2103@gmail.com>
2021-09-10 11:55:22 -0300

    tela inicio montada

---
+++ sistemazedococo/Inicio.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace sistemazedococo
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.btnPerfil = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.conteudos = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Image = global::sistemazedococo.Properties.Resources.logo_coco1;
+            this.logo.Location = new System.Drawing.Point(22, 14);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(105, 94);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            // 
+            // btnPerfil
+            // 
+            this.btnPerfil.BackColor = System.Drawing.Color.Transparent;
+            this.btnPerfil.BackgroundImage = global::sistemazedococo.Properties.Resources.btn_perfil;
+            this.btnPerfil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPerfil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPerfil.FlatAppearance.BorderSize = 0;
+            this.btnPerfil.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPerfil.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPerfil.Location = new System.Drawing.Point(636, 30);
+            this.btnPerfil.Name = "btnPerfil";
+            this.btnPerfil.Size = new System.Drawing.Size(76, 34);
+            this.btnPerfil.TabIndex = 1;
+            this.btnPerfil.Text = "\r\n";
+            this.btnPerfil.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.BackgroundImage = global::sistemazedococo.Properties.Resources.btn_sair;
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(721, 33);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(66, 28);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "\r\n";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // conteudos
+            // 
+            this.conteudos.BackColor = System.Drawing.Color.Transparent;
+            this.conteudos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.conteudos.Location = new System.Drawing.Point(0, 0);
+            this.conteudos.Name = "conteudos";
+            this.conteudos.Size = new System.Drawing.Size(816, 456);
+            this.conteudos.TabIndex = 2;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sistemazedococo.Properties.Resources.fundo_tela;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(816, 456);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnPerfil);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.conteudos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(816, 456);
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Button btnPerfil;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Panel conteudos;
+    }
+}
+
